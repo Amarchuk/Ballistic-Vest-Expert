1db3331dae5afe4932dee57579c1a4b997b43ac3
Amarchuk <aamarchuk@gmail.com>
2011-12-01 12:55:09 +0400

    Added methods for random question usage

--- src/Expert/ExpertWindow.java
+++ src/Expert/ExpertWindow.java
@@ -61,14 +61,14 @@ public class ExpertWindow {
         Vest b5 = new Vest("6Б5", "Общевойсковые бронежилеты 6Б5 (Ж-86) и 6Б5М серии \"Улей\"", "общевойсковой", "3", 2.7, 12.2, 49.6, 1986, "керамика", "http://www.russianarms.ru/forum/index.php/topic,1523.0.html", "imgs/6b5.jpg", "Имеет 18 модификаций");
         Vest b11 = new Vest("6Б11", "Общевойсковые бронежилеты 6Б11, 6Б12 и 6Б13 серии «Забрало»", "общевойсковой", "3", 4.2, 12.7, 50, 1999, "керамика", "http://www.russianarms.ru/forum/index.php/topic,1527.0.html", "imgs/6b11.jpg", "");
         Vest b15 = new Vest("6Б15", "Защитный комплект для экипажей бронированных машин 6Б15 «Ковбой»", "специальный", "1", 6.5, 6.5, 45, 2003, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1532.0.html", "imgs/6b15.jpg", "");
-        Vest b17 = new Vest("6Б17", "Бронежилеты специальные скрытного ношения 6Б17 и 6Б18","специальный", "1", 4.6, 5.0, 40, 2000, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1533.0.html", "imgs/6b15.jpg", "");
+        Vest b17 = new Vest("6Б17", "Бронежилеты специальные скрытного ношения 6Б17 и 6Б18", "специальный", "1", 4.6, 5.0, 40, 2000, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1533.0.html", "imgs/6b15.jpg", "");
         Vest b19 = new Vest("6Б19", "Бронежилет для наружных боевых постов кораблей и морской пехоты 6Б19", "специальный", "1", 2.8, 6.9, 28, 2003, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1534.0.html", "imgs/6b17.jpg", "Запас плавучести – от 3 до 6 кг");
         Vest b20 = new Vest("6Б20", "6Б20 - бронежилет для боевых пловцов ВМФ", "специальный", "1", 1, 1, 20, 2003, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1535.0.html", "imgs/6b20.jpg", "");
-        Vest b21 = new Vest("6Б21", "Боевые защитные комплекты 6Б21 (6Б21-1, 6Б21-2) и 6Б22 (6Б22-1, 6Б22-2) \"Пермячка-О\"", "специальный", "3",  8.5, 11.0, 160, 2004, "керамика",  "http://www.russianarms.ru/forum/index.php/topic,1536.0.html", "imgs/6b21.jpg", "Имеет пламезащитные рукава и штаны");
-        Vest b23 = new Vest("6Б23", "Общевойсковой бронежилет 6Б23 (6Б23-1, 6Б23-2)","общевойсковой", "3",3.6, 10.2, 48, 2003, "сталь", "http://www.russianarms.ru/forum/index.php/topic,1537.0.html", "imgs/6b23.jpg", "");
-        Vest b24 = new Vest("6Б24", "Бронекомплект для старшего командного состава 6Б24 \"Земляника-О\"","специальный", "2", 4.5, 5.0, 45, 2001, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1538.0.html",   "imgs/6b24.jpg", "Есть летний и зимний варианты");
-        Vest b25 = new Vest("6Б25", "Защитный комплект для экипажей бронированных машин ракетных войск и артиллерии 6Б25","специальный", "1",6.5, 6.5, 45, 2003, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1532.0.html",  "imgs/6b25.jpg", "Содержит огнезащитный костюм, вариант развития 6Б15 «Ковбой»");
-        Vest b43 = new Vest("6Б43", "Общевойсковой штурмовой бронежилет 6Б43 \"Забрало-Ш\"", "общевойсковой", "6а", 4.5,15.0, 68, 2010, "керамика", "http://www.russianarms.ru/forum/index.php/topic,6684.0.html?PHPSESSID=k9f2n8bgvcaoe2b9o145c7abk6","imgs/6b43.jpg", "");
+        Vest b21 = new Vest("6Б21", "Боевые защитные комплекты 6Б21 (6Б21-1, 6Б21-2) и 6Б22 (6Б22-1, 6Б22-2) \"Пермячка-О\"", "специальный", "3", 8.5, 11.0, 160, 2004, "керамика", "http://www.russianarms.ru/forum/index.php/topic,1536.0.html", "imgs/6b21.jpg", "Имеет пламезащитные рукава и штаны");
+        Vest b23 = new Vest("6Б23", "Общевойсковой бронежилет 6Б23 (6Б23-1, 6Б23-2)", "общевойсковой", "3", 3.6, 10.2, 48, 2003, "сталь", "http://www.russianarms.ru/forum/index.php/topic,1537.0.html", "imgs/6b23.jpg", "");
+        Vest b24 = new Vest("6Б24", "Бронекомплект для старшего командного состава 6Б24 \"Земляника-О\"", "специальный", "2", 4.5, 5.0, 45, 2001, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1538.0.html", "imgs/6b24.jpg", "Есть летний и зимний варианты");
+        Vest b25 = new Vest("6Б25", "Защитный комплект для экипажей бронированных машин ракетных войск и артиллерии 6Б25", "специальный", "1", 6.5, 6.5, 45, 2003, "ткань", "http://www.russianarms.ru/forum/index.php/topic,1532.0.html", "imgs/6b25.jpg", "Содержит огнезащитный костюм, вариант развития 6Б15 «Ковбой»");
+        Vest b43 = new Vest("6Б43", "Общевойсковой штурмовой бронежилет 6Б43 \"Забрало-Ш\"", "общевойсковой", "6а", 4.5, 15.0, 68, 2010, "керамика", "http://www.russianarms.ru/forum/index.php/topic,6684.0.html?PHPSESSID=k9f2n8bgvcaoe2b9o145c7abk6", "imgs/6b43.jpg", "");
 
         final HashMap map = new HashMap();
 
@@ -107,11 +107,11 @@ public class ExpertWindow {
         fieldsVest.put("specialTypeBox", "\"общевойсковой\" или \"специальный\"");
         fieldsVest.put("defenceBox", "\"защита от\"");
         fieldsVest.put("matherialBox", "материал");
-        fieldsVest.put( "armorTypeBox", "класс защиты");
-        fieldsVest.put( "weightMaxVal", "максимальный вес");
-        fieldsVest.put( "weightMinVal", "минимальный вес");
-        fieldsVest.put( "armorAreaVal", "площадь защиты (в дм2)");
-        fieldsVest.put( "yearDestribVal", "год принятия на вооружение");
+        fieldsVest.put("armorTypeBox", "класс защиты");
+        fieldsVest.put("weightMaxVal", "максимальный вес");
+        fieldsVest.put("weightMinVal", "минимальный вес");
+        fieldsVest.put("armorAreaVal", "площадь защиты (в дм2)");
+        fieldsVest.put("yearDestribVal", "год принятия на вооружение");
 
         changeStyle.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -126,7 +126,7 @@ public class ExpertWindow {
                     if (changeStyle.getSelectedItem() == "Обучение") {
                         Head.setText("Обучение");
                         setPicture("imgs/startPage.jpg");
-                        startTest.setText("Проверить ответ");
+                        startTest.setText("Получить вопрос");
                     }
                     if (changeStyle.getSelectedItem() == "Описание") {
                         Head.setFont(new Font(null, Font.BOLD, 16));
@@ -146,6 +146,12 @@ public class ExpertWindow {
 
 
         startTest.addActionListener(new ActionListener() {
+            Random i = new Random();
+            Vest request;
+            int curr = 0;
+            String trueAnswer = "";
+            String userAnswer = "";
+
             public void actionPerformed(ActionEvent e) {
                 if (showVast.getSelectedItem() == "..." && startTest.getText() == "Начать опознование") {
                     Vest request = new Vest("", "", (String) specialTypeBox.getSelectedItem(), (String) armorTypeBox.getSelectedItem(), Double.valueOf(weightMinVal.getText()), Double.valueOf(weightMaxVal.getText()), Double.valueOf(armorAreaVal.getText()), Integer.valueOf(yearDestribVal.getText()), (String) matherialBox.getSelectedItem(), "", "", "");  //TODO
@@ -156,25 +162,50 @@ public class ExpertWindow {
                 }
 
                 if (showVast.getSelectedItem() == "..." && startTest.getText() == "Показать параметры") {
-                   testResult.setText("Выберите бронежилет --->>>");
+                    testResult.setText("Выберите бронежилет --->>>");
                 } else if (startTest.getText() == "Показать параметры") {
                     Vest request = (Vest) map.get(showVast.getSelectedItem());
                     printVestAllInfo(request);
                 }
 
-                if (showVast.getSelectedItem() == "..." && startTest.getText() == "Проверить ответ") {
 
-                    Random i = new Random();
-                    Head.setFont(new Font(null, Font.BOLD, 16));
-                    Head.setText("Введите "+fieldsVest.get(fields.get(i.nextInt(8))) + " у бронежилета "+allVests[i.nextInt(14)]+".");
-
-                } else if (startTest.getText() == "Проверить ответ") {
-                    Random i = new Random();
-                    Head.setFont(new Font(null, Font.BOLD, 16));
-                    Vest request = (Vest) map.get(showVast.getSelectedItem());
-                    Head.setText("Введите "+fieldsVest.get(fields.get(i.nextInt(8))) + " у бронежилета "+request.nameShort +".");
+                if (startTest.getText() == "Получить вопрос" || startTest.getText() == "Проверить ответ") {
 
 
+                    if (showVast.getSelectedItem() == "..." && startTest.getText() == "Получить вопрос") {
+                        testResult.setText("");
+                        Head.setFont(new Font(null, Font.BOLD, 16));
+
+                        request = (Vest) map.get(allVests[i.nextInt(14)]);
+                        curr = i.nextInt(8);
+                        trueAnswer = getTrueAnswer(curr, request);
+
+                        Head.setText("Введите " + fieldsVest.get(fields.get(curr)) + " у бронежилета " + request.nameShort + ".");
+                        startTest.setText("Проверить ответ");
+
+                    } else if (startTest.getText() == "Получить вопрос") {
+                        testResult.setText("");
+                        Head.setFont(new Font(null, Font.BOLD, 16));
+
+                        request = (Vest) map.get(showVast.getSelectedItem());
+                        curr = i.nextInt(8);
+                        trueAnswer = getTrueAnswer(curr, request);
+                        Head.setText("Введите " + fieldsVest.get(fields.get(curr)) + " у бронежилета " + request.nameShort + ".");
+                        startTest.setText("Проверить ответ");
+
+                    } else if (startTest.getText() == "Проверить ответ") {
+
+                        userAnswer = getUserAnswer(curr);
+                        if (userAnswer.equals(trueAnswer)) {
+                            testResult.setText("Правильно! Получите следующий вопрос -->>>");
+                        } else {
+
+                            testResult.setText("Неверно: правильный ответ " + trueAnswer + "\n" + "Получите следующий вопрос -->>>");
+                        }
+
+                        startTest.setText("Получить вопрос");
+
+                    }
                 }
             }
         });
@@ -329,15 +360,75 @@ public class ExpertWindow {
 
     }
 
-    public void setRandomQuestion(Vest v){
-         if(v.nameShort == ""){
-             Head.setText("Текст вопроса");
-         }
+    public void setRandomQuestion(Vest v) {
+        if (v.nameShort == "") {
+            Head.setText("Текст вопроса");
+        }
 
     }
 
     public static void main(String[] args) {
         ExpertWindow w = new ExpertWindow();
     }
+
+
+    public String getTrueAnswer(int i, Vest v) {
+
+        HashMap def = new HashMap();
+        def.put("1", "осколки");
+        def.put("2", "пистолет Токарева");
+        def.put("3", "пистолет Токарева");
+        def.put("4", "автомат АК-74");
+        def.put("5", "автомат АК-74");
+        def.put("5а", "автомат АК-74");
+        def.put("6", "винтовка СВД");
+        def.put("6а", "винтовка СВД");
+
+        switch (i) {
+            case 0:
+                return v.specialType;
+            case 1:
+                return (String) def.get(v.armorClass);
+            case 2:
+                return v.matherial;
+            case 3:
+                return v.armorClass;
+            case 4:
+                return String.valueOf(v.weightMax);
+            case 5:
+                return String.valueOf(v.weightMin);
+            case 6:
+                return String.valueOf(v.armorArea);
+            case 7:
+                return String.valueOf(v.yearDestr);
+        }
+
+        return null;
+    }
+
+    public String getUserAnswer(int i) {
+
+        switch (i) {
+            case 0:
+                return (String) specialTypeBox.getSelectedItem();
+            case 1:
+                return (String) defenceBox.getSelectedItem();
+            case 2:
+                return (String) matherialBox.getSelectedItem();
+            case 3:
+                return (String) armorTypeBox.getSelectedItem();
+            case 4:
+                return weightMaxVal.getText();
+            case 5:
+                return weightMinVal.getText();
+            case 6:
+                return armorAreaVal.getText();
+            case 7:
+                return yearDestribVal.getText();
+        }
+
+        return null;
+    }
+
 
 }
